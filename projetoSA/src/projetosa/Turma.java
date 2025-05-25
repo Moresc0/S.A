cadd5319941855d215f12005b33d1c9d48bb95d1
Moresco <gustavomoresco00@gmail.com>
2025-05-25 14:38:17 -0300

    Versão 1.0

---
+++ projetoSA/src/projetosa/Turma.java
@@ -0,0 +1,22 @@
+package projetosa;
+
+public class Turma {
+    
+    private String nome;
+
+    public Turma() {
+    }
+
+    public Turma(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
